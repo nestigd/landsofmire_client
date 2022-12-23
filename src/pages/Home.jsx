6577db4af2aa460834db7298b05de3c291f21b43
nestigd <nestigd95@live.com>
2022-12-23 23:21:55 +0100

    added react-dom-router starting to create Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return(
+        <div>
+            <div>HERO IMAGE</div>
+            <div>Weekly sale - call to action</div>
+            <div>Cards</div>
+            <div>Go to shop</div>
+            <div>Footer</div>
+        </div>
+    );
+};
+
+export default Home

cd019d666dcf213a10ecfaccb14f7b6876a8db9b
nestigd <nestigd95@live.com>
2022-12-25 21:38:39 +0100

    finished styling cards

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -8,16 +8,19 @@ function  CardContainer (){
                 title= "Iron sword"
                 fullPrice = {12}
                 price = {8}
+                toExpiry= {5}
             />
             <HomepageCard
                 title= "Black steel hammer"
                 fullPrice = {24}
                 price = {16}
+                toExpiry= {1}
             />
             <HomepageCard
                 title= "Elven brass staff"
                 fullPrice = {38}
                 price = {20}
+                toExpiry= {10}
             />
         </div>
     )
--- src/components/HomepageCard.jsx
+++ src/components/HomepageCard.jsx
@@ -1,19 +1,19 @@
 import Loading from '../icons/loading.png'
 
 function  HomepageCard (props){
-    const {title, price, fullPrice} = props;
+    const {title, price, fullPrice, toExpiry} = props;
 
     return(
         <div className="homepage-card">
-            <span className="homepage-card--title"></span>
-            <img src={Loading} alt="loading image" />
-            <h2>{title}</h2>
-            <div className="homepage-card--prices">
-                <span>Previous: <strong className='msrp'>{fullPrice}$</strong></span>
-                <span>Current: <strong>{price}$</strong></span>
+            <img src={Loading} alt="loading" className='margin-m'/>
+            <h2 className='margin-xs'>{title}</h2>
+            <p className='margin-l'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos sint unde soluta sed cupiditate.</p>
+            <span className='countdown margin-xs'>Expires in {toExpiry} hours</span>           
+
+            <div className="homepage-card--prices margin-xs" >
+                <span className='msrp'>Price <strong>{fullPrice}$</strong></span>
+                <button>Buy {price}$</button>
             </div>
-            
-            <button>Purchase</button>
         </div>
     )
 }
